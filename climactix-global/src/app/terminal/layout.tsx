bd13ddea909f5cdb5b98e79f07855dd189a10644
rushikeshkulkarni163-blip <rushikeshkulkarni163@gmail.com>
2026-05-06 19:48:49 +0530

    climate intelligence

--- climactix-global/src/app/terminal/layout.tsx
+++ climactix-global/src/app/terminal/layout.tsx
@@ -1,21 +1,3 @@
-"use client";
-
-import TerminalSidebar from "@/components/terminal/TerminalSidebar";
-import TerminalTopBar from "@/components/terminal/TerminalTopBar";
-
 export default function TerminalLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex h-screen bg-white overflow-hidden">
-      {/* Left sidebar */}
-      <TerminalSidebar />
-
-      {/* Main panel */}
-      <div className="flex flex-col flex-1 min-w-0 overflow-hidden">
-        <TerminalTopBar />
-        <main className="flex-1 overflow-y-auto bg-[#F8F9FA]">
-          {children}
-        </main>
-      </div>
-    </div>
-  );
+  return <>{children}</>;
 }
--- climactix-global/src/components/layout/ConditionalLayout.tsx
+++ climactix-global/src/components/layout/ConditionalLayout.tsx
@@ -1,17 +1,7 @@
-"use client";
-
-import { usePathname } from "next/navigation";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 export default function ConditionalLayout({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
-  const isTerminal = pathname.startsWith("/terminal");
-
-  if (isTerminal) {
-    return <>{children}</>;
-  }
-
   return (
     <>
       <Navbar />
